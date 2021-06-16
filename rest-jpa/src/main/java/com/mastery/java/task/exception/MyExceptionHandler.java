579ffa0d4f272378a3100d31e90bced0c6845833
Vasili Kostopravov <3590291@mail.ru>
2021-06-17 00:00:50 +0300

    add @ResponseStatus annotations

--- rest-jpa/src/main/java/com/mastery/java/task/exception/MyExceptionHandler.java
+++ rest-jpa/src/main/java/com/mastery/java/task/exception/MyExceptionHandler.java
@@ -3,6 +3,7 @@ package com.mastery.java.task.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.UnexpectedTypeException;
@@ -13,21 +14,33 @@ import java.util.NoSuchElementException;
 public class MyExceptionHandler {
 
     @ExceptionHandler({NoSuchElementException.class,
-                        EmployeeNotFoundException.class,
                         UnexpectedTypeException.class,
                         MethodArgumentNotValidException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ApiException exceptionHandling(Exception e) {
-        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
 
         ApiException apiException = new ApiException(
                 e.getMessage(),
-                badRequest,
+                HttpStatus.BAD_REQUEST,
+                ZonedDateTime.now()
+        );
+        return apiException;
+    }
+
+    @ExceptionHandler({EmployeeNotFoundException.class})
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public ApiException handle(Exception e) {
+
+        ApiException apiException = new ApiException(
+                e.getMessage(),
+                HttpStatus.NOT_FOUND,
                 ZonedDateTime.now()
         );
         return apiException;
     }
 
     @ExceptionHandler({RuntimeException.class})
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
     public String anotherErrors() {
         return "Ooops! Something is wrong!";
     }
