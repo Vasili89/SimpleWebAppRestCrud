cca3bf270d6cf757b18a6f7bff04e7b2c0e97966
Vasili Kostopravov <3590291@mail.ru>
2021-06-19 00:19:05 +0300

    add some my methods to repository

--- rest-jpa/src/main/java/com/mastery/java/task/dao/EmployeeDao.java
+++ rest-jpa/src/main/java/com/mastery/java/task/dao/EmployeeDao.java
@@ -2,11 +2,21 @@ package com.mastery.java.task.dao;
 
 import com.mastery.java.task.dto.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface EmployeeDao extends JpaRepository<Employee, Long> {
+
+    Employee findByFirstNameAndLastName(String name1, String name2);
+
+    List<Employee> findByFirstNameStartingWithIgnoreCase(String str);
+
+    @Query("select e from Employee e where e.jobTitle = ?1")
+    List<Employee> findByJob(String jobTitle);
 
 }
 
